67ed7353e108a209c12cf4da92b572c3fe78e150
Drew Ku <andrewku123@gmail.com>
2023-08-18 16:11:59 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 # Do not run as root!
 
-hugo server --bind=0.0.0.0 --baseURL=https://drewku.com/ --appendPort=false --disableFastRender
+hugo server --bind=0.0.0.0 --baseURL=https://blog.drewku.com/ --appendPort=false --disableFastRender
